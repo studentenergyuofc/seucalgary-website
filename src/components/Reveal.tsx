2172ffc09e5def427b4f2fc13d68bc4c17671f01
Axeloooo <axelshz@gmail.com>
2023-10-13 18:14:25 -0600

    Fix padding for join us main container in App.css

--- src/components/Reveal.tsx
+++ src/components/Reveal.tsx
@@ -1,26 +1,40 @@
-import React, {useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 import { motion, useInView, useAnimation } from "framer-motion";
 
-interface Props{
-    children: JSX.Element;
-    width?: "fit-content" | "60%";
+interface Props {
+  children: JSX.Element;
+  width?: "fit-content" | "60%";
 }
 
-export const Reveal = ({children, width = "60%"}: Props) => {
-    const ref = useRef(null);
-    const isInView = useInView(ref, {once: true});
+export const Reveal = ({ children, width = "60%" }: Props) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
 
-    const mainControls = useAnimation();
+  const mainControls = useAnimation();
 
-    useEffect(() => {
-        if (isInView){
-            mainControls.start("visible");
-        }
-    }, [isInView]);
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView, mainControls]);
 
-    return(
-        <div ref={ref} className="mission-text" style={{position: "relative", width, overflow: "hidden"}}>
-            <motion.div variants={{hidden: {opacity:0, y:75}, visible: {opacity: 1, y:0}}} initial="hidden" animate={mainControls} transition={{duration: 0.8, delay: 0.30}}>{children}</motion.div>
-        </div>
-    );
+  return (
+    <div
+      ref={ref}
+      className="mission-text"
+      style={{ position: "relative", width, overflow: "hidden" }}
+    >
+      <motion.div
+        variants={{
+          hidden: { opacity: 0, y: 75 },
+          visible: { opacity: 1, y: 0 },
+        }}
+        initial="hidden"
+        animate={mainControls}
+        transition={{ duration: 0.8, delay: 0.3 }}
+      >
+        {children}
+      </motion.div>
+    </div>
+  );
 };
