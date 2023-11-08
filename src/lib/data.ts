eb6defaa6452c505b255e8a753ba2dac5d9e00f5
podgaietska <podgaietska.m@gmail.com>
2023-11-08 12:44:03 -0700

    scroll down functionality on main page

---
+++ src/lib/data.ts
@@ -0,0 +1,155 @@
+export const positions = [
+  {
+    title: "VP DEVELOPMENT",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "JR. DEVELOPMENT",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/computer-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "VP EVENTS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/video-camera-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "JR. EVENTS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/calender-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "VP PUBLIC RELATIONS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/megaphone-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "JR. PUBLIC RELATIONS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/at-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "VP COMMUNICATIONS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/chat-bubble-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+  {
+    title: "JR. COMMUNICATIONS",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    deadline: "September 15th 2023",
+    path: "/phone-missing-dynamic-color.png",
+    btnText: "Applications closed",
+    isEnable: false,
+  },
+] as const;
+
+export const upcomingEvents = [
+  {
+    title: "Upcoming 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-right",
+  },
+  {
+    title: "Upcoming 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-left",
+  },
+  {
+    title: "Upcoming 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-right",
+  },
+] as const;
+
+export const pastEvents = [
+  {
+    title: "Past 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-right",
+  },
+  {
+    title: "Past 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-left",
+  },
+  {
+    title: "Past 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-right",
+  },
+] as const;
+
+export const achievementsEvents = [
+  {
+    title: "Achievements 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-right",
+  },
+  {
+    title: "Achievements 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-left",
+  },
+  {
+    title: "Achievements 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae eros quis nisl aliquam aliquet.",
+    date: "September 15th 2023",
+    path: "/rocket-dynamic-color.png",
+    animation: "fade-right",
+  },
+] as const;
