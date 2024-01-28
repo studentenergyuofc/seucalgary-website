6c0a3c5ca8b775b548b23d14b344ee0ec61a9912
podgaietska <podgaietska.m@gmail.com>
2024-01-27 18:40:23 -0700

    fix lint #3

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,7 +2,12 @@ import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import DropDownMenu from "./DropDownMenu";
 
-function Layout({dropdownMenu, setDropdownMenu} : {dropdownMenu: boolean, setDropdownMenu: any}) {
+interface LayoutProps {
+  setDropdownMenu: (value: boolean) => void;
+  dropdownMenu: boolean;
+}
+
+function Layout({dropdownMenu, setDropdownMenu} : LayoutProps) {
   return (
     <div className="main">
       <Header setDropdownMenu={setDropdownMenu} dropdownMenu={dropdownMenu}/>
